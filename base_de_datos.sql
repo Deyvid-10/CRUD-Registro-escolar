27882467288a0fe86828f9bc3aea4984ab700c3e
Deyvid-10 <marmolejodeyvid@gmail.com>
2024-03-07 21:07:00 -0400

    .

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `registro_escolar` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `registro_escolar`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: registro_escolar
@@ -56,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-03 20:29:25
+-- Dump completed on 2024-03-07 21:05:23
